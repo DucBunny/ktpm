1d3d6ef6ad77f05db85374edd4a67270dbd1ce20
quyetpm2004 <phammanhquyetx9@gmail.com>
2025-06-10 18:12:20 +0700

    add: collection period

--- src/main/java/com/app/controllers/Payments/CollectionPeriodsController.java
+++ src/main/java/com/app/controllers/Payments/CollectionPeriodsController.java
@@ -23,7 +23,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Objects;
 
-public class PaymentsController {
+public class CollectionPeriodsController {
     private String role;
     private String username;
 

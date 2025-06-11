3523af5bef1f5b86afe8dd586e2647a5d6ad1d57
Quyet Pham <phammanhquyetx9@gmail.com>
2025-06-12 00:59:59 +0700

    update chart homepage

--- src/main/java/com/app/controllers/HomePageController.java
+++ src/main/java/com/app/controllers/HomePageController.java
@@ -4,12 +4,21 @@ import com.app.controllers.Payments.CollectionPeriods.CollectionPeriodsControlle
 import com.app.controllers.Residents.ResidentsController;
 import com.app.controllers.Revenues.RevenuesController;
 import com.app.controllers.Rooms.RoomsController;
+import com.app.models.CollectionPeriods;
+import com.app.utils.CustomAlert;
+import com.app.utils.DatabaseConnection;
 import com.app.utils.SceneNavigator;
 import com.app.utils.StageManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
@@ -17,6 +26,9 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Objects;
 
 public class HomePageController {
@@ -33,8 +45,10 @@ public class HomePageController {
     private AnchorPane mainContentPane;
     @FXML
     private HBox footerBar;
+    @FXML
+    private BarChart<String, Number> chart;
 
-    public void initialize(String role, String username) {
+    public void initialize(String role, String username) throws IOException {
         this.role = role;
         this.username = username;
 
@@ -49,6 +63,7 @@ public class HomePageController {
         }
 
         nameLabel.setText("Xin chào, " + username);
+        initBarChart();
     }
 
     // Header Button -----------------------------------------------------------
@@ -106,4 +121,60 @@ public class HomePageController {
         Stage owner = StageManager.getPrimaryStage();
         SceneNavigator.showPopupScene("/fxml/Revenues/create-revenue.fxml", "/styles/Revenues/crud-revenue.css", owner);
     }
+
+    public void initBarChart() throws IOException {
+
+        // Khởi tạo 2 series
+        XYChart.Series<String, Number> seriesTotal = new XYChart.Series<>();
+        XYChart.Series<String, Number> seriesPaid = new XYChart.Series<>();
+
+        seriesTotal.setName("Tổng số tiền");
+        seriesPaid.setName("Số tiền đã đóng");
+
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String query = """
+            SELECT
+                cp.name,
+                COALESCE(SUM(ci.total_amount), 0) AS total_amount,
+                COALESCE((
+                    SELECT SUM(p.amount)
+                    FROM payments p
+                    WHERE p.collection_period_id = cp.id
+                ), 0) AS total_paid_amount
+            FROM
+                collection_periods cp
+            LEFT JOIN
+                collection_items ci ON cp.id = ci.collection_period_id
+            GROUP BY
+                cp.id, cp.name, cp.start_date, cp.end_date, cp.type
+            ORDER BY
+                cp.start_date DESC;
+        """;
+
+            PreparedStatement stmt = connection.prepareStatement(query);
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int totalAmount = resultSet.getInt("total_amount");
+                int totalPaidAmount = resultSet.getInt("total_paid_amount");
+                seriesTotal.getData().add(new XYChart.Data<>(name, totalAmount));
+                seriesPaid.getData().add(new XYChart.Data<>(name, totalPaidAmount));
+            }
+
+            chart.getData().clear();
+            chart.getData().setAll(seriesTotal, seriesPaid);
+
+
+            // Cài đặt khoảng cách giữa các cột
+            chart.setCategoryGap(10);
+            chart.setBarGap(2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            CustomAlert.showErrorAlert("Không thể tải dữ liệu đợt thu từ CSDL.");
+        }
+    }
+
+
 }
